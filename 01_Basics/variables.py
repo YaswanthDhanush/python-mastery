06e0453587ba2a8d63d80d3b67b297321a8486b6
Yaswanth <yaswanthdhanush16@gmail.com>
2026-07-14 12:46:45 +0530

    Day 1 - Variables Practice

--- 01_Basics/variables.py
+++ 01_Basics/variables.py
@@ -10,3 +10,26 @@ print("Age : ", age)
 print("Salary : ", salary)
 print("Company : ", company)
 
+city = 'Bangalore'
+country = 'India'
+salary  = 5000
+experience = 4.7
+is_working = 'Y'
+
+print("City : ", city)
+print("County : ", country)
+print("Salary : ", salary)
+print("Experience : ", experience)
+print("Working ? : " ,is_working)
+
+favourite_actor = 'Dhanush'
+favourite_movie = 'Asuran'
+dream_company = 'Accenture'
+dream_salary = '2L per Month'
+favourite_bike = 'MT15'
+
+print("fav actor : ", favourite_actor)
+print("fav movie : ", favourite_movie)
+print("dream cmpny : ", dream_company)
+print("dream Salary :", dream_salary)
+print("fav bike : ", favourite_bike)

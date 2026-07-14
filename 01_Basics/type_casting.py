08ecdce1cebb469194e9909ceaaffb826c45dde7
Yaswanth <yaswanthdhanush16@gmail.com>
2026-07-14 14:00:31 +0530

    Day 1 - Type Casting

---
+++ 01_Basics/type_casting.py
@@ -0,0 +1,24 @@
+name = "Yaswanth"
+age = "26"
+is_working = "True"
+salary = 20000
+experience  = "4.7"
+
+print(name)
+print(type(name))
+
+age = int(age)
+print(age)
+print(type(age))
+
+is_working = bool(is_working)
+print(is_working)
+print(type(is_working))
+
+salary = float(salary)
+print(salary)
+print(type(salary))
+
+experience = float(experience)
+print(experience)
+print(type(experience))

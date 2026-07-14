9093bbf1a60d8ab0cf57930cf36007210b961d7a
Yaswanth <yaswanthdhanush16@gmail.com>
2026-07-14 12:25:17 +0530

    Day 1 - Variables

---
+++ 01_Basics/variables.py
@@ -0,0 +1,12 @@
+name = 'Yaswanth'
+role = 'Data Quality Engineer'
+age = 26
+salary = 2000
+company = 'Google'
+
+print("Name : " , name)
+print("Role : " , role)
+print("Age : ", age)
+print("Salary : ", salary)
+print("Company : ", company)
+

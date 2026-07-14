fb25454f882e5582b0397bf09fc2cea154bc5648
Yaswanth <yaswanthdhanush16@gmail.com>
2026-07-14 13:24:10 +0530

    Day 1 - data_types

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,43 @@
+name = 'Yaswanth'
+age = 26
+city = 'Blr'
+experience = 4.6
+salary = 2000
+is_working = True
+fav_actor = 'Dhanush'
+fav_movie = 'Asuran'
+bike = 'MT15'
+dream_company = 'Accenture'
+
+print(name)
+print(age)
+print(city)
+print(experience)
+print(salary)
+print(is_working)
+print(fav_actor)
+print(fav_movie)
+print(bike)
+print(dream_company)
+
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(experience))
+print(type(salary))
+print(type(is_working))
+print(type(fav_actor))
+print(type(fav_movie))
+print(type(bike))
+print(type(dream_company))
+
+print(isinstance(name, str))
+print(isinstance(age, int))
+print(isinstance(city, str))
+print(isinstance(experience, float))
+print(isinstance(salary, int))
+print(isinstance(is_working, bool))
+print(isinstance(fav_actor, str))
+print(isinstance(fav_movie, str))
+print(isinstance(bike , str))
+print(isinstance(dream_company, str))

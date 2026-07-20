ec9eb22d156a914e509cd9e7bd4813a617deb659
Yaswanth <yaswanthdhanush16@gmail.com>
2026-07-20 15:03:12 +0530

    day 3 leanings - User Input

---
+++ 01_Basics/input_value.py
@@ -0,0 +1,17 @@
+'''Write a program that:
+Asks the user for:
+Name
+Age
+Salary
+Convert:
+Age → int
+Salary → float
+'''
+name = input("Enter the name : ")
+age = int(input("Enter the age : "))
+salary = float(input("Enter the salary : "))
+
+print("Name is : ", name)
+print("Age is : ",age)
+print("Salary is : ",salary)
+
